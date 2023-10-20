c87c771aaa9581a8ef196a5be762eba077e0d692
Dominux <kir.gimranov@gmail.com>
2023-10-20 16:06:16 +0300

    tried spawning manager with channels

--- pentaract/src/main.rs
+++ pentaract/src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use axum::{routing::get, Router};
+use tokio::sync::{mpsc, oneshot};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 const ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8080);
@@ -16,13 +17,41 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    let (tx, mut rx) = mpsc::channel::<Responder<String>>(32);
+
+    // running manager
+    tokio::spawn(async move {
+        tracing::debug!("manager ran");
+
+        // Start receiving messages
+        while let Some(resp_rx) = rx.recv().await {
+            tracing::debug!("got task");
+
+            let _ = resp_rx.send("lmao".to_string());
+        }
+    });
+
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route(
+        "/",
+        get(|| async move {
+            let (resp_tx, resp_rx) = oneshot::channel();
+
+            tracing::debug!("sending task to manager");
+            let _ = tx.send(resp_tx).await;
+
+            resp_rx.await.unwrap()
+        }),
+    );
 
     // run it
-    tracing::debug!("listening on {ADDR}");
+    tracing::debug!("listening on http://{ADDR}");
     axum::Server::bind(&ADDR)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
+
+/// Provided by the requester and used by the manager task to send
+/// the command response back to the requester.
+type Responder<T> = oneshot::Sender<T>;
