6b904101eb38ebb7f720da0dcc34fad91ad6f1fc
Dominux <kir.gimranov@gmail.com>
2023-11-26 01:31:11 +0300

    #16: created login page layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,7 +6,6 @@ import Login from "./pages/Login";
 const App: Component = () => {
   return (
     <>
-      <h1>My Site with Lots of Pages</h1>
       <Routes>
         <Route path="/login" component={Login} />
         <Route path="/" element={<Navigate href={"/login"} />}></Route>
--- ui/src/pages/Login.tsx
+++ ui/src/pages/Login.tsx
@@ -1,7 +1,38 @@
 import { Component } from "solid-js";
+import Container from "@suid/material/Container";
+import Box from "@suid/material/Box";
+import TextField from "@suid/material/TextField";
+import Button from "@suid/material/Button";
+import Paper from "@suid/material/Paper";
+import Typography from "@suid/material/Typography";
+import Divider from "@suid/material/Divider";
 
 const Login: Component = () => {
-  return <div>Hey</div>;
+  return (
+    <Container maxWidth="sm" sx={{ width: "fit-content" }}>
+      <Paper sx={{ mt: "20vh" }} elevation={4}>
+        <Box
+          component="form"
+          sx={{
+            px: 5,
+            py: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            "& > :not(style)": { my: 1.5 },
+          }}
+          noValidate
+        >
+          <Typography variant="h5">Pentaract Account</Typography>
+          <Divider />
+          <TextField label="Username" variant="standard" />
+          <TextField label="Password" variant="standard" type="password" />
+          <Divider />
+          <Button variant="contained">Login</Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Login;
